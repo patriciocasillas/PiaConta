c5d8909b4de7f8b24bc80d61393e520e450a440f
Raul Arturo Salomon Iglesias <73568954+RaulSalomonIgl@users.noreply.github.com>
2020-11-16 18:13:53 -0600

    Update Presupuesto_Financiero.py
    Reestructuración del funcionamiento

--- Presupuesto_Financiero.py
+++ Presupuesto_Financiero.py
@@ -1,12 +1,16 @@
 ##Estado de Costo de Producción y Ventas
 def SCPS():
     print()
+    input("presiona la tecla Enter para continuar")
 ##Estado de Resultados
 def Statement_Income():
     print()
+    input("presiona la tecla Enter para continuar")
 ##Estado de Flujo de Efectivo
 def Cash_Flow_Statement():
     print()
+    input("presiona la tecla Enter para continuar")
 ##Balance General
 def Balance_Sheet():
     print()
+    input("presiona la tecla Enter para continuar")
